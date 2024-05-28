572ada104737c4f1bbda41ff141bc0a12edd874f
Edvin23 <150404642+Edvin23@users.noreply.github.com>
2024-05-27 19:11:57 -0700

    Add files via upload

---
+++ emonzon10.py
@@ -0,0 +1,14 @@
+#Edvin Monzon
+#CSC-321
+#Lab 10
+
+def area(baseCalc,baseheight):
+    return (base * height)
+
+base = int(input("Enter the base of the rectangle: "))
+
+height = int(input("Enter the height of the rectangle: "))
+
+rectangle = area(base, height)
+
+print("The area of the rectangle is: ", rectangle)
